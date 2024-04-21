96310a5ea0414b5172e84840574bf216a0e5b185
Dilip_Choudhary <choudhary.29@iitj.ac.in>
2024-04-21 15:08:21 +0530

    photos added

---
+++ component/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="logo.png" alt="Logo" />
+      </div>
+      <h1 className="navbar-heading">Your Heading Here</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
